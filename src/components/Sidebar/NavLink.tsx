7f4055284b34d42beacde06f941a4c5c7ccecf18
guilherme <guilherme.godoi@allcomsistemas.com.br>
2021-07-29 17:05:05 -0300

    :sparkles: Adicionado Chakra UI

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,5 +1,19 @@
+import { Icon, Link, Text } from '@chakra-ui/react';
+import { ElementType } from 'react';
+
+interface INavLink {
+  icon: ElementType;
+}
+
 const NavLink: React.FC = () => {
-  return <div />;
+  return (
+    <Link display="flex" align="center" color="pink.400">
+      <Icon as={} fontSize="20" />
+      <Text ml="4" fontWeight="medium">
+        Dashboard
+      </Text>
+    </Link>
+  );
 };
 
 export default NavLink;
